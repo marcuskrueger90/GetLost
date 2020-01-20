05bad4d6f6adfb6c64dbe620a0b8d059bc97bff3
Justin Carlson <justincarlson7@gmail.com>
2020-01-19 23:55:29 -0500

    update schema file to support additional funtionality such as creating trips

--- models/User.js
+++ models/User.js
@@ -18,7 +18,77 @@ const UserSchema = new Schema({
   date: {
     type: Date,
     default: Date.now
+  },
+  phone: {
+    type: String,
+    required: false
+  },
+  owner_id: {
+    type: String,
+    required: false
+  },
+  user_id: {
+    type: Schema.ObjectId,
+    auto: true,
+    required: true
+  },
+  trip: {
+    location: [
+      {
+        address: {
+          type: String
+        },
+        city: {
+          type: String
+        },
+        state: {
+          type: String
+        },
+        zip: {
+          type: String
+        }
+      }
+    ],
+
+    tripEndDateTime: {
+      type: Date
+    },
+    pin: {
+      type: Number
+    },
+    alarmDispatched: {
+      type: Boolean,
+      default: false
+    },
+    alarm_id: {
+      type: String
+    }
   }
 });
 
 module.exports = User = mongoose.model("users", UserSchema);
+
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// // Create Schema
+// const UserSchema = new Schema({
+//   name: {
+//     type: String,
+//     required: true
+//   },
+//   email: {
+//     type: String,
+//     required: true
+//   },
+//   password: {
+//     type: String,
+//     required: true
+//   },
+//   date: {
+//     type: Date,
+//     default: Date.now
+//   }
+// });
+
+// module.exports = User = mongoose.model("users", UserSchema);
