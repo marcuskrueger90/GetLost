447c59a27090b777170fe4a53ae1e1a8195c816b
Justin Carlson <justincarlson7@gmail.com>
2020-01-20 15:36:44 -0500

    cron function working, updated App.js, Cronfuction.js, package.json

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import PrivateRoute from "./components/private-route/PrivateRoute";
 import Dashboard from "./components/dashboard/Dashboard";
+import Cronjobfunc from "./components/Cronfunction";
 
 import "./App.css";
 
@@ -37,6 +38,7 @@ if (localStorage.jwtToken) {
 }
 class App extends Component {
   render() {
+    Cronjobfunc();
     return (
       <Provider store={store}>
         <Router>
