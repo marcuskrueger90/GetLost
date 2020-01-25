d529de580db43b65c394b06fb0d88128aca29ac6
Ian Applekamp <ieapplekamp@gmail.com>
2020-01-25 10:13:23 -0500

    added nexmo for a texting and a background for landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,20 +1,60 @@
 import React, { Component } from "react";
+import "./Landing.css"
+import Particles from 'react-particles-js';
+
 // import { Link } from "react-router-dom";
 
 class Landing extends Component {
   render() {
     return (
-      <div className="">
-        <div className="">
-          <div className="">
-            <h4>
-              This is the Landing/Home Page
-            </h4>
-            <p>
-              Full-stack MERN app with user authentication via
-              passport and JWTs
-            </p>
+      <div >
+        <div className="background-sky">
+        <Particles
+            params={{
+              "particles": {
+                  "number": {
+                      "value": 200,
+                      "density": {
+                          "enable": true,
+                          "value_area": 1500
+                      }
+                  },
+                  "line_linked": {
+                      "enable": true,
+                      "opacity": 0.02
+                  },
+                  "move": {
+                      "direction": "right",
+                      "speed": 0.08
+                },
+                "color": {
+                    "value": "#fff"
+                },
+                  "size": {
+                      "value": 1.5
+                  },
+                  "opacity": {
+                      "anim": {
+                          "enable": true,
+                          "speed": .2,
+                          "opacity_min": 0.05
+                      }
+                  }
+              },
+              "retina_detect": true
+          }} />
+          </div>
+         <div className="background-mountain">
+           <div className="">
+             <h4>
+               This is the Landing/Home Page
+             </h4>
+             <p>
+               Full-stack MERN app with user authentication via
+               passport and JWTs
+             </p>
             <br />
+            {/* <img className="background-pics" src={require('../../images/sky.png')} alt="dawn pic"></img> */}
             {/* <div className="">
               <Link to="/register"className="">Register</Link>
             </div>
@@ -22,8 +62,8 @@ class Landing extends Component {
               <Link to="/login" className="">Log In</Link>
     </div> */}
           </div>
-        </div>
-      </div>
+         </div>
+       </div>
     );
   }
 }
