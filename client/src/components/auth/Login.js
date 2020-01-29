13264b6de684aa496d9705c1091f388e36eb6f38
Ian Applekamp <ieapplekamp@gmail.com>
2020-01-29 16:19:26 -0500

    upped the particles and added some styling to the dash

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -63,7 +63,7 @@ class Login extends Component {
             params={{
               "particles": {
                   "number": {
-                      "value": 200,
+                      "value": 300,
                       "density": {
                           "enable": true,
                           "value_area": 1500
--- client/src/components/layout/Creators.js
+++ client/src/components/layout/Creators.js
@@ -15,7 +15,7 @@ class Creators extends Component {
             params={{
               "particles": {
                   "number": {
-                      "value": 200,
+                      "value": 300,
                       "density": {
                           "enable": true,
                           "value_area": 1500
